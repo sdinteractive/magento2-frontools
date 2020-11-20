f69c363b3a6adebc72b5e2818dc2ba7e84dfe5d6
Kristian Charbonneau <kcharbonneau@somethingdigital.com>
2020-11-20 17:47:31 -0500

    Update gulpfile to use gulp-task-loader helper

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,19 @@
 'use strict';
 // Plugins / Functions / Modules
 const plugins = require('gulp-load-plugins')({
-        pattern: ['*', '!gulp', '!gulp-load-plugins'],
-        rename : {
-          'browser-sync'    : 'browserSync',
-          'fs-extra'        : 'fs',
-          'gulp-multi-dest' : 'multiDest',
-          'js-yaml'         : 'yaml',
-          'marked-terminal' : 'markedTerminal',
-          'merge-stream'    : 'mergeStream',
-          'postcss-reporter': 'reporter',
-          'run-sequence'    : 'runSequence'
-        }
-      }),
-      config = {};
+    pattern: ['*', '!gulp', '!gulp-load-plugins'],
+    rename : {
+      'browser-sync'    : 'browserSync',
+      'fs-extra'        : 'fs',
+      'gulp-multi-dest' : 'multiDest',
+      'js-yaml'         : 'yaml',
+      'marked-terminal' : 'markedTerminal',
+      'merge-stream'    : 'mergeStream',
+      'postcss-reporter': 'reporter',
+      'run-sequence'    : 'runSequence'
+    }
+  }),
+  config = {};
 
 config.projectPath = plugins.fs.realpathSync('../../../') + '/';
 config.tempPath = config.projectPath + 'var/view_preprocessed/frontools';
@@ -23,7 +23,7 @@ plugins.errorMessage = require('./helper/error-message')(plugins);
 plugins.getThemes = require('./helper/get-themes')(plugins, config);
 
 // Tasks loading
-require('gulp-task-loader')({
+require('./helper/gulp-task-loader')({
   dir    : 'task',
   plugins: plugins,
   configs: config
